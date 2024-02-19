84e1df75505f8a4108e8af82f6e5075e9c6c49b6
OGnewar <85093314+OGnewar@users.noreply.github.com>
2024-02-19 10:43:49 +0545

    Update fullAdder_tb.vhdl

--- fullAdder/fullAdder_tb.vhdl
+++ fullAdder/fullAdder_tb.vhdl
@@ -1,45 +1,92 @@
-library	IEEE;
-use	IEEE.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-entity	fa_test	is
-end	fa_test;
+entity tb_full_adder is
+end tb_full_adder;
 
-architecture	test_bench	of	fa_test	is
-component	full_adder	is
-Port	(A,B,Cin	:	in	STD_LOGIC;
-	S,C	:	out	STD_LOGIC);
-end	component	full_adder;
+architecture testbench of tb_full_adder is
 
-signal	A,B,Cin,S,C:	STD_LOGIC;
+    -- Component declaration for the DUT (Design Under Test)
+    component Full_Adder is
+        Port (
+            A : in  STD_LOGIC;
+            B : in  STD_LOGIC;
+            CIN : in  STD_LOGIC;
+            S : out  STD_LOGIC;
+            COUT : out  STD_LOGIC
+        );
+    end component;
+
+    -- Signals for test bench
+    signal A_tb, B_tb, CIN_tb, S_tb, COUT_tb : STD_LOGIC;
 
 begin
 
-uut	:	full_adder	port	map(
-A=>A,	B=>B,
-Cin=>Cin,S=>S,C=>C);
+    -- Instantiate the Full_Adder DUT
+    UUT : Full_Adder
+    port map (
+        A => A_tb,
+        B => B_tb,
+        CIN => CIN_tb,
+        S => S_tb,
+        COUT => COUT_tb
+    );
 
-stim:	process
-begin
-A<='0';
-B<='0';
-Cin<='0';
-wait	for	10	ns;
+    -- Stimulus process
+    stimulus : process
+    begin
+        -- Test case 1: A = '0', B = '0', CIN = '0'
+        A_tb <= '0';
+        B_tb <= '0';
+        CIN_tb <= '0';
+        wait for 10 ns;
 
-A<='0';
-B<='0';
-Cin<='1';
-wait	for	10	ns;
+        -- Test case 2: A = '0', B = '1', CIN = '0'
+        A_tb <= '0';
+        B_tb <= '1';
+        CIN_tb <= '0';
+        wait for 10 ns;
 
-A<='0';
-B<='1';
-Cin<='0';
-wait	for	10	ns;
+        -- Test case 3: A = '1', B = '1', CIN = '1'
+        A_tb <= '1';
+        B_tb <= '1';
+        CIN_tb <= '1';
+        wait for 10 ns;
 
-A<='0';
-B<='1';
-Cin<='1';
-wait	for	10	ns;
-	wait;
+            -- Test case 4: A = '1', B = '0', CIN = '0'
+        A_tb <= '1';
+        B_tb <= '0';
+        CIN_tb <= '0';
+        wait for 10 ns;
 
-end	process;
-end	test_bench;
+        -- Test case 5: A = '0', B = '1', CIN = '1'
+        A_tb <= '0';
+        B_tb <= '1';
+        CIN_tb <= '1';
+        wait for 10 ns;
+
+        -- Test case 6: A = '1', B = '0', CIN = '1'
+        A_tb <= '1';
+        B_tb <= '0';
+        CIN_tb <= '1';
+        wait for 10 ns;
+
+        -- Test case 7: A = '1', B = '1', CIN = '0'
+        A_tb <= '1';
+        B_tb <= '1';
+        CIN_tb <= '0';
+        wait for 10 ns;
+
+        -- Test case 8: A = '1', B = '1', CIN = '1'
+        A_tb <= '1';
+        B_tb <= '1';
+        CIN_tb <= '1';
+        wait for 10 ns;
+
+
+        -- Add more test cases as needed...
+
+        wait;
+    end process stimulus;
+
+end testbench;
