dc4cd4aded70da3d3602e4362047066e816cea22
OGnewar <85093314+OGnewar@users.noreply.github.com>
2024-02-19 10:44:29 +0545

    Update fullAdder.vhdl

--- fullAdder/fullAdder.vhdl
+++ fullAdder/fullAdder.vhdl
@@ -1,65 +1,63 @@
---	vhdl code for or gate
-LIBRARY	IEEE;
-USE	IEEE.STD_LOGIC_1164.ALL;
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
 
-ENTITY	or_gate	IS
-PORT(
-	x,y:IN	STD_LOGIC;
-	z:OUT	STD_LOGIC
-);
-END	or_gate;
+entity Full_Adder is
+  port
+  (
+    A    : in std_logic;
+    B    : in std_logic;
+    CIN  : in std_logic;
+    S    : out std_logic;
+    COUT : out std_logic
+  );
+end Full_Adder;
 
-ARCHITECTURE	or_arch	OF	or_gate	IS
-BEGIN
-	z<=x	or	y;
-END	or_arch;
+architecture Behavioral of Full_Adder is
 
--- vhdl code for half adder
-LIBRARY	IEEE;
-USE	IEEE.STD_LOGIC_1164.ALL;
+  signal S1, C1, C2 : std_logic;
 
-ENTITY	half_adder	IS
-PORT(
-	a,b:IN	STD_LOGIC;
-	s,c:OUT	STD_LOGIC
-);
-END	half_adder;
+  component OR_Gate is
+    port
+    (
+      A : in std_logic;
+      B : in std_logic;
+      Y : out std_logic
+    );
+  end component;
 
-ARCHITECTURE	ha_arch	OF	half_adder	IS
-BEGIN
-s<=a	xor	b;
-c<=a	and	b;
-END	ha_arch;
+  component Half_Adder is
+    port
+    (
+      A : in std_logic;
+      B : in std_logic;
+      S : out std_logic;
+      C : out std_logic
+    );
+  end component;
 
---vhdl code for full adder using two half adder and a or gate
+begin
 
-LIBRARY	IEEE;
-USE	IEEE.STD_LOGIC_1164.ALL;
+  HA1 : entity work.Half_Adder port map
+    (
+    A => A,
+    B => B,
+    S => S1,
+    C => C1
+    );
 
-ENTITY	full_adder	IS
-PORT(
-	A,B,Cin:IN	STD_LOGIC;
-	S,C:OUT	STD_LOGIC
-);
-END	full_adder;
-ARCHITECTURE	fa_arch	OF	full_adder	IS
-COMPONENT	or_gate	IS
-PORT(
-	x,y:IN	STD_LOGIC;
-	z:OUT	STD_LOGIC			
-);
-END	COMPONENT	or_gate;
+  HA2 : entity work.Half_Adder port
+    map (
+    A => S1,
+    B => CIN,
+    S => S,
+    C => C2
+    );
 
-COMPONENT	half_adder	IS
-PORT(
-	a,b:IN	STD_LOGIC;
-	s,c:OUT	STD_LOGIC
-);
-END	COMPONENT	half_adder;
+  G1 : entity work.OR_Gate port
+    map (
+    A => C1,
+    B => C2,
+    Y => COUT
+    );
 
-SIGNAL	S1,C1,C2:STD_LOGIC;
-BEGIN
-	HA1:half_adder	PORT	MAP(a=>A,b=>B,s=>S1,c=>C1);
-	HA2:half_adder	PORT	MAP(a=>S1,b=>Cin,s=>S,c=>C2);
-	OR1:or_gate	PORT	MAP(x=>C1,y=>C2,z=>C);
-END	fa_arch;
+end Behavioral;
